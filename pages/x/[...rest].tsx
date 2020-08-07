897fd6c9d638be1e7b58ade6da31ad4f1bfa5555
Qu4k <pippo.reds@gmail.com>
2020-08-07 12:19:15 +0200

    fix: remove mock version table

--- pages/x/[...rest].tsx
+++ pages/x/[...rest].tsx
@@ -105,29 +105,6 @@ function Module() {
         </div>
       </Header>
       <Main>
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 col-gap-4 row-gap-8">
-          <div>
-            <h4 className="font-bold text-gray-400">
-              {versionInfo && versionInfo.versions.length} releases:
-            </h4>
-            <table className="border-collapse">
-              <tbody>
-                <tr>
-                  <th className="pr-2 text-right font-medium">1.12.2</th>
-                  <td className="justify text-gray-500">x, nest</td>
-                </tr>
-                <tr>
-                  <th className="pr-2 text-right font-medium">1.12.1</th>
-                  <td className="justify text-gray-500">x</td>
-                </tr>
-                <tr>
-                  <th className="pr-2 text-right font-medium">1.12.0</th>
-                  <td className="justify text-gray-500">x</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
         {readme ? (
           <Markdown
             source={readme.source}
